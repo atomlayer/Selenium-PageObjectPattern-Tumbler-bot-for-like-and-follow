7094c23b75902e9beb9dbf990f1fd8d91ccb797b
AtomLayer <anfulu36484@gmail.com>
2019-02-24 15:43:58 +0300

    save and load settings

--- TumblerBot_Selenium_Test/MainWindow.xaml.cs
+++ TumblerBot_Selenium_Test/MainWindow.xaml.cs
@@ -23,11 +23,49 @@ namespace TumblerBot_Selenium_Test
     {
         private Logger Logger;
 
+
         public MainWindow()
         {
             InitializeComponent();
             Logger = LogManager.GetLogger("log");
+            LoadSettings();
         }
 
+        public void LoadSettings()
+        {
+            Settings settings = Settings.LoadSettings();
+            MaxCountOfLikePerDayTextBox.Text =settings.MaxCountOfLikePerDayText;
+            MaxCountOfLikePerUserTextBox.Text=settings.MaxCountOfLikePerUserText;
+            MaxCountOfFollowPerDayTextBox.Text=settings.MaxCountOfFollowPerDayText;
+            SearchWordsTextBox.Text=settings.SearchWordsText;
+        }
+
+        public Settings GetSettings()
+        {
+            Settings settings =new Settings();
+            try
+            {
+                settings.MaxCountOfLikePerDayText=MaxCountOfLikePerDayTextBox.Text;
+                settings.MaxCountOfLikePerUserText = MaxCountOfLikePerUserTextBox.Text;
+                settings.MaxCountOfFollowPerDayText= MaxCountOfFollowPerDayTextBox.Text;
+                settings.SearchWordsText = SearchWordsTextBox.Text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return settings;
+        }
+
+        private void SaveSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            GetSettings().SaveSettings();
+        }
+
+        private void LoadDefaultSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.GetDefaultSettings().SaveSettings();
+            LoadSettings();
+        }
     }
 }
--- TumblerBot_Selenium_Test/Settings.cs
+++ TumblerBot_Selenium_Test/Settings.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -9,7 +10,7 @@ using Newtonsoft.Json;
 namespace TumblerBot_Selenium_Test
 {
     [JsonObject(MemberSerialization.OptIn)]
-    class Settings
+    public class Settings
     {
         [JsonProperty]
         public int MaxCountOfLikePerDay;
@@ -21,20 +22,97 @@ namespace TumblerBot_Selenium_Test
         public int MaxCountOfFollowPerDay;
 
         [JsonProperty]
-        public List<string> searchwords;
+        public List<string> SearchWords;
 
-        private string _settingsFile="settings.json";
+        private static string _settingsFile=@"settings.json";
+
+
+        public string SearchWordsText
+        {
+            set
+            {
+                if(value=="")
+                    throw new Exception("Invalid data format for SearchWords");
+                try { SearchWords = value.Split(',').ToList(); }
+                catch { throw new Exception("Invalid data format for SearchWords");}
+            }
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                if (SearchWords == null) return sb.ToString();
+                for (int i = 0; i < SearchWords.Count; i++)
+                {
+                    sb.Append(SearchWords[i]);
+                    if (i != SearchWords.Count - 1) sb.Append(", ");
+                }
+                return sb.ToString();
+            }
+        }
+
+        public string MaxCountOfFollowPerDayText
+        {
+            set
+            {
+                try { MaxCountOfFollowPerDay = Convert.ToInt32(value); }
+                catch { throw new Exception("Invalid data format for  MaxCountOfFollowPerDay "); }
+                if( MaxCountOfFollowPerDay<=0)
+                    throw new Exception("Invalid data format for  MaxCountOfFollowPerDay ");
+             }
+            get { return MaxCountOfFollowPerDay.ToString(); }
+        }
+
+        public string MaxCountOfLikePerUserText
+        {
+            set
+            {
+                try { MaxCountOfLikePerUser = Convert.ToInt32(value); }
+                catch { throw new Exception("Invalid data format for  MaxCountOfLikePerUser");}
+                if(MaxCountOfLikePerUser<=0)
+                    throw new Exception("Invalid data format for  MaxCountOfFollowPerDay ");
+            }
+            get { return MaxCountOfLikePerUser.ToString(); }
+        }
+
+        public string MaxCountOfLikePerDayText
+        {
+            set
+            {
+                try { MaxCountOfLikePerDay = Convert.ToInt32(value); }
+                catch { throw new Exception("Invalid data format for  MaxCountOfLikePerDay"); }
+                if(MaxCountOfLikePerDay<=0)
+                    throw new Exception("Invalid data format for  MaxCountOfLikePerDay");
+            }
+            get { return MaxCountOfLikePerDay.ToString(); }
+        }
+
+
 
         public void SaveSettings()
         {
             string serialize = JsonConvert.SerializeObject(this);
-            File.WriteAllText(Path.Combine(System.IO.Directory.GetCurrentDirectory()+_settingsFile), serialize);
+            File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory + _settingsFile), serialize);
         }
 
-        public Settings LoadSettings()
+        public static Settings LoadSettings()
         {
-            string settings = File.ReadAllText(Path.Combine(System.IO.Directory.GetCurrentDirectory() + _settingsFile));
-            return JsonConvert.DeserializeObject<Settings>(settings);
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + _settingsFile);
+            if (!File.Exists(path))
+            {
+                GetDefaultSettings().SaveSettings();
+            }
+            return JsonConvert.DeserializeObject<Settings>(File.ReadAllText(path));
+        }
+
+        public static Settings GetDefaultSettings()
+        {
+            Settings settings = new Settings()
+            {
+                MaxCountOfLikePerDay = 1000,
+                MaxCountOfLikePerUser = 3,
+                MaxCountOfFollowPerDay = 200,
+                SearchWords = new List<string> {"test1", "test2"}
+            };
+            return settings;
         }
     }
 }
